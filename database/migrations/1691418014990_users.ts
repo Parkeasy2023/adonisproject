a66d9b03041250bdb21edab65d6dd772ac91f5ec
Parkeasy2023 <parkeasylpa@gmail.com>
2023-09-19 16:01:46 -0300

    Alteração da lenght do doc

--- database/migrations/1691418014990_users.ts
+++ database/migrations/1691418014990_users.ts
@@ -10,7 +10,7 @@ export default class extends BaseSchema {
       table.string('usuario',255).notNullable().unique()
       table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
-      table.string('document', 11).notNullable().unique()
+      table.string('document', 18).notNullable().unique()
       table.string('telefone', 11).notNullable().unique()
       table.string('sexo', 255).nullable() 
       table.string('datanasc', 10).nullable()
