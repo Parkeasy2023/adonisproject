389af649ef9bbdc061c2641bba81a8a192b49d07
Parkeasy2023 <parkeasylpa@gmail.com>
2023-11-06 09:26:41 -0300

    VeiculoValidator

---
+++ app/Validators/VeiculoValidator.ts
@@ -0,0 +1,30 @@
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class VeiculoValidator {
+  constructor(protected ctx: HttpContextContract) { }
+  public schema = schema.create({
+    placa: schema.string({}, [
+      rules.required(),
+      rules.unique({ table: 'Veiculos', column: 'placa' })
+    ]),
+    cor: schema.string({}, [ //deve ser select
+      rules.required(),
+    ]),
+    marca: schema.string({}, [
+      rules.required()
+    ]),
+    password: schema.string({}, [
+      rules.required(),
+      rules.minLength(5)
+    ]),
+    modelo: schema.string({}, [
+      rules.required()
+    ]),
+  })
+
+  public messages: CustomMessages = {
+    required: "O {{field}} é obrigatório!",
+    'placa.unique': "Veículo já cadastrado!",
+  }
+}
