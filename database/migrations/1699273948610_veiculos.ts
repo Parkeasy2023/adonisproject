007018eb3e7669dd31dfdd97b0cb7862271b6c97
Parkeasy2023 <parkeasylpa@gmail.com>
2023-11-06 10:18:03 -0300

    table.string('token', 64).notNullable().unique()  em migration

--- database/migrations/1699273948610_veiculos.ts
+++ database/migrations/1699273948610_veiculos.ts
@@ -13,7 +13,6 @@ export default class extends BaseSchema {
       table.string('cor').notNullable()
       table.string('marca').notNullable()
       table.string('modelo').notNullable()
-      table.string('token', 64).notNullable().unique()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
